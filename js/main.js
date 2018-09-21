9bf0ab3a936577f22006c5c9e6680773195f1d03
ThugLifeChief <winter.sebastian15@gmail.com>
2018-09-21 22:21:23 +0200

    challenge 6 complete

--- js/main.js
+++ js/main.js
@@ -2,7 +2,22 @@ console.log('app is alive');
 
 //Switch Channels
 function switchChannel (channelName) {
-    console.log('Turning into channel ' + channelName)
+    console.log('Turning into channel ' + channelName);
     $('#app-bar-name').html(channelName);
     $('#app-bar-location').html(' by <strong><a href="https://map.what3words.com/upgrading.never.helps">upgrading.never.helps</a></strong>');
+    $('#app-bar-star img').attr('src','http://ip.lfe.mw.tum.de/sections/star-o.png');
+}
+
+function changeappbarstar () {
+    $('#app-bar-star img').attr('src','http://ip.lfe.mw.tum.de/sections/star.png');
+}
+
+function selectTab (selectedTab){
+    $('#tab-bar button').removeClass('selected');
+    $('#'+selectedTab).addClass('selected');
+    console.log('new selected Tab is ' + selectedTab);
+}
+
+function toggleEmojibox(){
+    $('#emojis').toggle();
 }
